d0c005c6784eabb0557e582a8ac0cd0d7814b569
NaNkeen <ck210400@protonmail.ch>
2019-12-21 16:44:13 +0000

    Removed .d files and adding aarch32 boot code

---
+++ kernel/arch/aarch32/boot/boot.s
@@ -0,0 +1,50 @@
+// AArch32 boot section
+.section ".text.boot"
+
+.global _start
+.type _start, @function
+_start:
+				// Halt co-processors
+				mrc p15, #0, r1, c0, c0, #5
+				and r1, r1, #3
+				cmp r1, #0
+				bne halt	
+
+				// Setup stack register
+				ldr sp, =_start
+				
+				// BSS initialization
+				ldr r4, =__bss_start
+				ldr r9, =__bss_end
+
+				// ===== BEGIN CLEAR BSS =====
+				ldr r4, =__bss_start
+				ldr r9, =__bss_end
+				mov r5, #0
+				mov r6, #0
+				mov r7, #0
+				mov r8, #0
+				b       2f
+			 
+			1:
+				// store multiple at r4.
+				stmia r4!, {r5-r8}
+			 
+				// If we are still below bss_end, loop.
+			2:
+				cmp r4, r9
+				blo 1b
+			 
+				// ===== END CLEAR BSS =====
+
+				// Initialize kernel and start main process
+				ldr r3, =ker_init
+				blx r3
+				
+				ldr r3, =ker_main
+				blx r3
+			 
+				// halt
+			halt:
+				wfe
+				b halt
--- kernel/kernel/kernel.d
+++
@@ -1,5 +0,0 @@
-kernel/kernel.o: kernel/kernel.c include/kernel/tty.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/stddef.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/stdint.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/9.2.0/include/stdint-gcc.h \
- include/i386/vga.h
